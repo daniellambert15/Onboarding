826462a539f6e5a8b916f0abcd3e353c36210132
daniel <daniel@rawrsome.co.uk>
2016-11-10 18:53:53 +0000

    modifying quiz

--- resources/views/quizzes.blade.php
+++ resources/views/quizzes.blade.php
@@ -26,7 +26,7 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @foreach($user->quizzes->where('submitted', null) as $quiz)
+                                        @foreach($user->quizzes as $quiz)
                                             <tr>
                                                 <td>
                                                     {{ $quiz->id }}
